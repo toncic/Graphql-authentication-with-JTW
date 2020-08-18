40ca155752f2072db546ae350892e69b08eb2d72
toncic <milan.toncic@wunder.io>
2020-08-18 22:00:35 +0300

    Setting up apollo-server-express.

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,48 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+// import {createConnection} from "typeorm";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { Query, buildSchema, Resolver } from 'type-graphql';
 
-createConnection().then(async connection => {
+// import {User} from "./entity/User";
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+// createConnection().then(async connection => {
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+//     console.log("Inserting a new user into the database...");
+//     const user = new User();
+//     user.firstName = "Timber";
+//     user.lastName = "Saw";
+//     user.age = 25;
+//     await connection.manager.save(user);
+//     console.log("Saved a new user with id: " + user.id);
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+//     console.log("Loading users from the database...");
+//     const users = await connection.manager.find(User);
+//     console.log("Loaded users: ", users);
 
-}).catch(error => console.log(error));
+//     console.log("Here you can setup and run express/koa/any other framework.");
+
+// }).catch(error => console.log(error));
+
+@Resolver()
+class Hello {
+    @Query(() => String)
+    async hello () {
+        return "Hello World";
+    }
+}
+
+(  async () => {
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [Hello]
+        })
+    });
+
+    apolloServer.applyMiddleware({ app });
+    app.listen(4000, () => {
+        console.log("Express server started at localhost:4000");
+    });
+})();
